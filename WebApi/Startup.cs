26df1a21fdeb764d569911b383ad61cd84061803
Czarnuk <robdar987@gmail.com>
2019-05-18 12:28:45 +0100

    Update Startup.cs
    Update Startup.cs

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApi.Models;
 
 namespace WebApi
 {
@@ -25,6 +27,7 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
